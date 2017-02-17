810eef734c5606a3aae10e2dddf43497f3401024
Nguyen Duc Tam <ndtam.273@gmail.com>
2017-02-18 02:20:07 +0900

    init prj

---
+++ SwiftTest/Supports/SwiftTest-Bridging-Header.h
@@ -0,0 +1,13 @@
+//
+//  SwiftTest-Bridging-Header.h
+//  SwiftTest
+//
+//  Created by Nguyen Duc Tam on 2017/02/17.
+//  Copyright © 2017年 Nguyen Duc Tam. All rights reserved.
+//
+
+#ifndef SwiftTest_Bridging_Header_h
+#define SwiftTest_Bridging_Header_h
+
+
+#endif /* SwiftTest_Bridging_Header_h */
