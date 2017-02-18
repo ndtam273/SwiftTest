306fbb881a0b5d6f8ec91fbb1e26aa3929cb3015
Nguyen Duc Tam <ndtam.273@gmail.com>
2017-02-19 00:49:25 +0900

    remove storyboard

---
+++ SwiftTest/Controllers/Home/HomeVC.swift
@@ -0,0 +1,25 @@
+//
+//  HomeVC.swift
+//  SwiftTest
+//
+//  Created by Nguyen Duc Tam on 2017/02/18.
+//  Copyright © 2017年 Nguyen Duc Tam. All rights reserved.
+//
+
+import UIKit
+
+class HomeVC: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
---
+++ SwiftTest/Model/Character.swift
@@ -0,0 +1,9 @@
+//
+//  Character.swift
+//  SwiftTest
+//
+//  Created by Nguyen Duc Tam on 2017/02/18.
+//  Copyright © 2017年 Nguyen Duc Tam. All rights reserved.
+//
+
+import Foundation
